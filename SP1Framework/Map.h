7463c278fe91a32f9f80f0c3c5d2634aae3d6d4b
Joashfeh <63983498+Joashfeh@users.noreply.github.com>
2020-08-18 15:15:15 +0800

    enum map floor

--- SP1Framework/Map.h
+++ SP1Framework/Map.h
@@ -4,29 +4,29 @@
 #include <time.h>
 
 enum LEVEL {
-	ONE,
-	TWO,
-	THREE,
-	FOUR,
-	FIVE,
-	SIX,
-	SEVEN,
-	EIGHT,
-	NINE,
-	TEN,
-	ELEVEN,
-	TWELVE
+	ONE = 1,
+	TWO = 2,
+	THREE = 3,
+	FOUR = 4,
+	FIVE = 5,
+	SIX = 6,
+	SEVEN = 7,
+	EIGHT = 8,
+	NINE = 9,
+	TEN = 10,
+	ELEVEN = 11,
+	TWELVE = 12
 };
 
 class Map {
 public:
-	int floor;
+	LEVEL floor;
 	Area* map[6][6]; // 4x4 map array but taken as 6x6 for camera tracking
 	int fullMap[96][96]; // upscaled to display onto the screen ( 2 x 4 )
 	int display[96][196];
 
 	Map();
-	Map(int f, int floorType); // floorType: 0 for Tutorial, 1 for normal dungeons, 2 for bosses
+	Map(LEVEL f, int floorType); // floorType: 0 for Tutorial, 1 for normal dungeons, 2 for bosses
 	~Map();
 
 	void generateDungeon();
