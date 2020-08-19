912211a62373b43059f332826d536aa2c0e445fb
lyx-poly <201992Y@mymail.nyp.edu.sg>
2020-08-19 17:28:06 +0800

    Changes to Enemy.cpp
    Learning about getting specific lines from txt file.

--- SP1Framework/Enemy.cpp
+++ SP1Framework/Enemy.cpp
@@ -1,6 +1,9 @@
 #include "Enemy.h"
+#include <iostream>
 #include <fstream>
+#include <string>
 
+using namespace std;
 
 
 int Enemy::enemyCount = 0;
@@ -15,7 +18,10 @@ void Enemy::Attack(Entity* ptrEntity)
 
 void Enemy::setenemystats(LEVEL f, int get_b) //
 {
-	std::string enemy_file_extract;
+	int line_no = 0;
+	string enemy_file_extract;
+	ifstream read;
+	//Reference: https://stackoverflow.com/questions/26288145/how-to-read-a-specific-line-from-file-using-fstream-c
 	switch (f)
 	{
 	case ONE: // SAMPLE CASE | Tutorial level 1 | JAN
@@ -23,8 +29,28 @@ void Enemy::setenemystats(LEVEL f, int get_b) //
 		switch (get_b)
 		{
 		case 1: //Dummy1
-			enemy_file_extract = "Enemy_data/level_01";
+
+			// First part sets health
+			read.open("Enemy_data/level_01/health_data.txt");
+
+			while (line_no != 0 && getline(read, enemy_file_extract))
+			{
+				++line_no;
+			}
+
+			if (line_no == 0) 
+			{
+				// sLine contains that line in the file.
+			}
+			else 
+			{
+				// The file contains fewer than said number of lines.
+			}
+
+			read.close(); // Closes file
+
 			break;
+
 		case 2: //Dummy2
 			break;
 		case 3: //Dummy3
--- SP1Framework/enemyDeclare.h
+++ SP1Framework/enemyDeclare.h
@@ -2,6 +2,7 @@
 #include "Enemy.h"
 #include "Map.h"
 
+
 Enemy* entity_ptrs[3];
 
 void enemyDeclare(Map& map);
