d2436ffd5d9139d43622b2a923949f0da0ae9dae
lyx-poly <201992Y@mymail.nyp.edu.sg>
2020-08-19 16:11:24 +0800

    Changes to Enemy.cpp and enemyDeclare.cpp

--- SP1Framework/Enemy.cpp
+++ SP1Framework/Enemy.cpp
@@ -1,4 +1,7 @@
 #include "Enemy.h"
+#include <fstream>
+
+
 
 Enemy::Enemy()
 {
@@ -8,21 +11,25 @@ void Enemy::Attack(Entity* ptrEntity)
 {
 }
 
-void Enemy::setenemystats(LEVEL f, int get_a) //
+void Enemy::setenemystats(LEVEL f, int get_b) //
 {
+	std::string enemy_file_extract;
 	switch (f)
 	{
-	case ONE: // SAMPLE CASE
+	case ONE: // SAMPLE CASE | Tutorial level 1 | JAN
 
-		switch (f)
+		switch (get_b)
 		{
 		case 1: //Dummy1
+			enemy_file_extract = "Enemy_data/level_01";
+			break;
+		case 2: //Dummy2
+			break;
+		case 3: //Dummy3
 			break;
 		default:
 			break;
 		}
-		HP = 15;
-		Damage = 10;
 
 	default:
 		// Default case | Should'nt be called
