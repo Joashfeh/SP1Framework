673ffad3a92cf35c7e31d8950d44ada813bbe4d0
lyx-poly <201992Y@mymail.nyp.edu.sg>
2020-08-19 10:06:08 +0800

    Added for loop to enemy.cpp.
    FOR loop uses enemycount constant to set stats for all three mobs per level.

--- SP1Framework/Enemy.cpp
+++ SP1Framework/Enemy.cpp
@@ -6,15 +6,18 @@ Enemy::Enemy()
 
 void Enemy::setenemystats(Map type)
 {
-	switch (type.floor)
+	for (int a = 0; a < enemycount; a++)
 	{
-	case ONE: // SAMPLE CASE
-		HP = 15;
-		Damage = 10;
-		Defence = 5;
-	default:
-		// Default case | Should'nt be called
-		break;
+		switch (type.floor)
+		{
+		case ONE: // SAMPLE CASE
+			HP = 15;
+			Damage = 10;
+			Defence = 5;
+		default:
+			// Default case | Should'nt be called
+			break;
+		}
 	}
 }
 
--- SP1Framework/Enemy.h
+++ SP1Framework/Enemy.h
@@ -7,6 +7,8 @@ using namespace std;
 class Enemy : public Entity
 {
 public:
+	const int enemycount = 3; // Max 3 mobs per level
+
 	Enemy();
 	void setenemystats(Map type); // Sets enemy stats based on type of enemy | Enemy type stored in this function
 	~Enemy();
