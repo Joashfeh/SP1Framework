6d4813b726c2dc92d1b5b0a7d677db2a6bae8672
ErnestToh <ernesttoh18@gmail.com>
2020-08-19 15:55:24 +0800

    weapon and armor data

--- SP1Framework/Weapon.cpp
+++ SP1Framework/Weapon.cpp
@@ -1,1 +1,20 @@
 #include "Weapon.h"
+#include <string>
+
+std::string Weapon[7] =
+{
+	"weapon1",
+	"weapon2",
+	"weapon3",
+	"weapon4",
+	"weapon5"
+};
+
+Weapon::Weapon(std::string name) {
+	
+}
+
+Weapon::~Weapon()
+{
+
+}
--- SP1Framework/Weapon.h
+++ SP1Framework/Weapon.h
@@ -1,7 +1,15 @@
 #pragma once
+#include <string>
 #include "Item.h"
 class Weapon :
     public Item
 {
+public:
+    int dmg;
+    int cost;
+
+    Weapon(std::string name);
+    ~Weapon();
+
 };
 
