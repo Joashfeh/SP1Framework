8f9e4e4a219aea40affde0759f5e74ce7eb92577
lyx-poly <201992Y@mymail.nyp.edu.sg>
2020-08-19 18:02:02 +0800

    Last comit for 19082020

--- SP1Framework/Enemy.cpp
+++ SP1Framework/Enemy.cpp
@@ -18,51 +18,42 @@ void Enemy::Attack(Entity* ptrEntity)
 
 void Enemy::setenemystats(LEVEL f, int get_b) //
 {
-	int line_no = 0;
-	string enemy_file_extract;
-	ifstream read;
-	//Reference: https://stackoverflow.com/questions/26288145/how-to-read-a-specific-line-from-file-using-fstream-c
-	switch (f)
-	{
-	case ONE: // SAMPLE CASE | Tutorial level 1 | JAN
+	string enemyDir;
+	enemyDir = "Enemy_Data/level_";
 
-		switch (get_b)
-		{
-		case 1: //Dummy1
 
-			// First part sets health
-			read.open("Enemy_data/level_01/health_data.txt");
 
-			while (line_no != 0 && getline(read, enemy_file_extract))
-			{
-				++line_no;
-			}
+	enemyDir += to_string(get_b);
 
-			if (line_no == 0) 
-			{
-				// sLine contains that line in the file.
-			}
-			else 
-			{
-				// The file contains fewer than said number of lines.
-			}
+	// Set Health
+	string enemyDirHP = enemyDir;
+	enemyDirHP += "/health_data.txt";
 
-			read.close(); // Closes file
+	std::ifstream enemyDataHP(enemyDirHP, std::ios::in);
+	if (!enemyDataHP)
+		return;
 
-			break;
+	string enemyset;
+	getline(enemyDataHP, enemyset);
 
-		case 2: //Dummy2
-			break;
-		case 3: //Dummy3
-			break;
-		default:
-			break;
-		}
+	// STOI: https://www.geeksforgeeks.org/converting-strings-numbers-cc/
+	HP = stoi(enemyset);
 
-	default:
-		// Default case | Should'nt be called
-		break;
-	}
+	// Set Damage
+	string enemyDirDamage = enemyDir;
+	enemyDirDamage += "/attack_data.txt";
+
+	std::ifstream enemyDataDamage(enemyDirDamage, std::ios::in);
+	if (!enemyDataDamage)
+		return;
+
+	enemyset = " "; // Clear enemyset
+	getline(enemyDataDamage, enemyset);
+
+	Damage = stoi(enemyset);
+
+	enemyDataHP.close();
+	enemyDataDamage.close();
 }
 
 /*int Enemy::return_enemy_count()
