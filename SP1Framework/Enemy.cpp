5c1ff427f429c197350426c0e504158918138e7a
lyx-poly <201992Y@mymail.nyp.edu.sg>
2020-08-24 17:33:33 +0800

    Removed setenemystats()

--- SP1Framework/Enemy.cpp
+++ SP1Framework/Enemy.cpp
@@ -28,45 +28,6 @@ void Enemy::Attack(Entity* ptrEntity, Console& g_Console) {
 	triggerRenderPlayerDamage();
 }
 
-/*void Enemy::setenemystats(int get_a) //
-{
-	string enemyDir;
-	enemyDir = "Enemy_Data/level_";
-	enemyDir += to_string(get_a + 1);
-
-
-	// Set Health
-	string enemyDirHP = enemyDir;
-	enemyDirHP += "/health_data.txt";
-
-	std::ifstream enemyDataHP(enemyDirHP, std::ios::in);
-	if (!enemyDataHP)
-		return;
-
-	string enemyset;
-	getline(enemyDataHP, enemyset);
-
-	// STOI: https://www.geeksforgeeks.org/converting-strings-numbers-cc/
-	HP = stoi(enemyset);
-
-
-	// Set Damage
-	string enemyDirDamage = enemyDir;
-	enemyDirDamage += "/attack_data.txt";
-
-	std::ifstream enemyDataDamage(enemyDirDamage, std::ios::in);
-	if (!enemyDataDamage)
-		return;
-
-	enemyset = " "; // Clear enemyset
-	getline(enemyDataDamage, enemyset);
-
-	Damage = stoi(enemyset);
-
-	enemyDataHP.close();
-	enemyDataDamage.close();
-}*/
-
 Enemy Enemy::loadEnemy(int level, int get_i)
 {
 	string enemyDir;
