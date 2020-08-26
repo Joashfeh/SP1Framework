5b124cb74c60469c7abb7484c9c4fd75ea14873a
Fou <therealjo3d3su@gmail.com>
2020-08-26 14:38:42 +0800

    weapon and armor sprites scaled up by 2 to make inventory not so empty

--- SP1Framework/Inventory.cpp
+++ SP1Framework/Inventory.cpp
@@ -24,11 +24,11 @@ void displayStats(Console& g_Console, Player& plr)
 void renderWeaponSprite(Console& g_Console, Player& plr)
 {
 
-	g_Console.writeToBuffer(100, 2, "Weapon");
+	g_Console.writeToBuffer(90, 2, "Weapon");
 	if (plr.mainWeapon.id == 0)
-		g_Console.writeToBuffer(101, 3, "None");
+		g_Console.writeToBuffer(91, 3, "None");
 	else
-		g_Console.writeToBuffer(103 - plr.mainWeapon.name.length() / 2, 3, plr.mainWeapon.name);
+		g_Console.writeToBuffer(93 - plr.mainWeapon.name.length() / 2, 3, plr.mainWeapon.name);
 
 	std::string WeaponSpriteFile = "Sprite_data/Weapon_sprite/Weapon0";
 	WeaponSpriteFile += std::to_string(plr.mainWeapon.id);
@@ -39,8 +39,8 @@ void renderWeaponSprite(Console& g_Console, Player& plr)
 		return;
 
 	COORD weapon;
-	weapon.X = 95;
-	weapon.Y = 5;
+	weapon.X = 77;
+	weapon.Y = 6;
 
 	COLOURS outputColour;
 
@@ -50,21 +50,28 @@ void renderWeaponSprite(Console& g_Console, Player& plr)
 		std::string a;
 		std::getline(WeaponSprite, a);
 
-		for (int col = 0; col < 8; col++) {
-			switch (a[col])
-			{
-			case '0': outputColour = COLOURS::BLACK;
-				break;
-			case '1': outputColour = COLOURS::WHITE;
-				break;
+
+		for (int i = 0; i < 2; i++) {
+			for (int col = 0; col < 8; col++) {
+				switch (a[col])
+				{
+				case '0': outputColour = COLOURS::BLACK;
+					break;
+				case '1': outputColour = COLOURS::WHITE;
+					break;
+				}
+				g_Console.writeToBuffer(weapon, " ", outputColour);
+				weapon.X++;
+				g_Console.writeToBuffer(weapon, " ", outputColour);
+				weapon.X++;
+				g_Console.writeToBuffer(weapon, " ", outputColour);
+				weapon.X++;
+				g_Console.writeToBuffer(weapon, " ", outputColour);
+				weapon.X++;
 			}
-			g_Console.writeToBuffer(weapon, " ", outputColour);
-			weapon.X++;
-			g_Console.writeToBuffer(weapon, " ", outputColour);
-			weapon.X++;
+			weapon.X = 77;
+			weapon.Y++;
 		}
-		weapon.X = 95;
-		weapon.Y++;
 	}
 
 
@@ -73,11 +80,11 @@ void renderWeaponSprite(Console& g_Console, Player& plr)
 
 void renderArmorSprite(Console& g_Console, Player& plr)
 {
-	g_Console.writeToBuffer(56, 2, "Armour");
+	g_Console.writeToBuffer(46, 2, "Armour");
 	if (plr.mainArmor.id == 0)
-		g_Console.writeToBuffer(57, 3, "None");
+		g_Console.writeToBuffer(47, 3, "None");
 	else
-		g_Console.writeToBuffer(59 - plr.mainArmor.name.length() / 2, 3, plr.mainArmor.name);
+		g_Console.writeToBuffer(49 - plr.mainArmor.name.length() / 2, 3, plr.mainArmor.name);
 
 	std::string ArmorSpriteFile = "Sprite_data/Armor_sprite/Armor0";
 	ArmorSpriteFile += std::to_string(plr.mainArmor.id);
@@ -88,8 +95,8 @@ void renderArmorSprite(Console& g_Console, Player& plr)
 		return;
 
 	COORD armor;
-	armor.X = 51;
-	armor.Y = 5;
+	armor.X = 33;
+	armor.Y = 6;
 
 	COLOURS outputColour = COLOURS::WHITE;
 
@@ -98,21 +105,28 @@ void renderArmorSprite(Console& g_Console, Player& plr)
 		std::string a;
 		std::getline(ArmorSprite, a);
 
-		for (int col = 0; col < 8; col++) {
-			switch (a[col])
-			{
-			case '0': outputColour = COLOURS::BLACK;
-				break;
-			case '1': outputColour = COLOURS::WHITE;
-				break;
+		for (int i = 0; i < 2; i++) {
+
+			for (int col = 0; col < 8; col++) {
+				switch (a[col])
+				{
+				case '0': outputColour = COLOURS::BLACK;
+					break;
+				case '1': outputColour = COLOURS::WHITE;
+					break;
+				}
+				g_Console.writeToBuffer(armor, " ", outputColour);
+				armor.X++;
+				g_Console.writeToBuffer(armor, " ", outputColour);
+				armor.X++;
+				g_Console.writeToBuffer(armor, " ", outputColour);
+				armor.X++;
+				g_Console.writeToBuffer(armor, " ", outputColour);
+				armor.X++;
 			}
-			g_Console.writeToBuffer(armor, " ", outputColour);
-			armor.X++;
-			g_Console.writeToBuffer(armor, " ", outputColour);
-			armor.X++;
+			armor.X = 33;
+			armor.Y++;
 		}
-		armor.X = 51;
-		armor.Y++;
 	}
 
 
@@ -122,8 +136,8 @@ void renderArmorSprite(Console& g_Console, Player& plr)
 void renderInventory(Console& g_Console, Player& plr, double dt)
 {
 	mainDisplay(g_Console);
-	displayBoxes(g_Console, 93, 4, 20, 10);
-	displayBoxes(g_Console, 49, 4, 20, 10);
+	displayBoxes(g_Console, 73, 4, 40, 20);
+	displayBoxes(g_Console, 29, 4, 40, 20);
 	renderWeaponSprite(g_Console, plr);
 	renderArmorSprite(g_Console, plr);
 	renderCharacterSprite(g_Console, 7, 5, plr, dt);
