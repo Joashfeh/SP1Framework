984f954c22b3cebc8ca57948190ea02d103ce0c2
Joashfeh <joashfeh3252@hotmail.com>
2020-08-27 15:19:47 +0800

    fixed reset

--- SP1Framework/updateGameOver.cpp
+++ SP1Framework/updateGameOver.cpp
@@ -1,8 +1,10 @@
 #include "updateGameOver.h"
+#include "Enemy.h"
 
 void updateGameOver(SKeyEvent* g_skKeyEvent, EGAMESTATES& g_eGameState, Player& plr) {
 	if (g_skKeyEvent[K_SPACE].keyDown) {
 		plr.reset();
+		Enemy::enemyCount = 0;
 		init();
 		g_eGameState = S_GAME;
 	}
