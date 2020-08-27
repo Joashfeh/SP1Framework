60204fc20d015b4d833a0572120da2350e9e1ea1
lyx-poly <201992Y@mymail.nyp.edu.sg>
2020-08-27 12:21:05 +0800

    Added a defence mechanism utilising floor
    You can revert this back to the old one if it's too op.

--- SP1Framework/Enemy.cpp
+++ SP1Framework/Enemy.cpp
@@ -4,7 +4,8 @@
 #include <fstream>
 #include <string>
 #include "renderCharacterDamage.h"
-#include "enemyAttackPatterns.h"
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 using namespace std;
 
@@ -25,13 +26,73 @@ Enemy::Enemy()
 void Enemy::Attack(Entity* ptrEntity, Console& g_Console) {
 
 	float damage = this->Damage;
+	//### SWITCH CASES FOR PATTERNS AND ABILITIES ###
+
+	srand(time(0)); // Generate a random number less than MSP for deciding chance for every turn.
+	float chance = rand() % total_stats_points;
 
 	// Pattern determinant switch
 	switch (pattern_selection)
 	{
+		//attack 0 | Does not attack | Usually not called
+	case 0:
+		//0 Damage | Usually not called as all enemies by default have pattern set to 1
+		damage = 0;
+		break;
+
+		//attack 1 | Attack only
+	case 1:
+		// Case 1 is left blank so that damage is unaltered
+		break;
+
+		//attack 2 | No attack
+	case 2:
+		if (chance < damage)
+		{
+			damage = 0;
+			break;
+		}
+		else
+		{
+			break;
+		}
+
+		//attack 3 | Double damage
+	case 3:
+		if (chance < damage)
+		{
+			damage *= 2;
+			break;
+		}
+		else
+		{
+			break;
+		}
+
 	default:
 		break;
 	}
+
+	chance = rand() % total_stats_points; // Generate new chance number for enemy to decide to use ability or to attack.
+
+	switch (ability_selection)
+	{
+	case 0:
+		// No abilities
+		break;
+	case 1:
+		// Full block chance with formula: defence / total_stats_points
+
+	case 2:
+
+	case 3:
+
+	default:
+		break;
+	}
+
+	//### END OF SWITCH CASES ###
+
 
 	if (((Player*)ptrEntity)->isDefend) {
 		damage = (float)damage;
@@ -43,8 +104,8 @@ void Enemy::Attack(Entity* ptrEntity, Console& g_Console) {
 		ptrEntity->HP -= damage;
 
 	if (ptrEntity->Defense != 0) {
-		ptrEntity->HP -= damage * 0.5;
-		ptrEntity->Defense -= damage * 0.5;
+		ptrEntity->HP -= floor(damage / ptrEntity->Defense);
+		ptrEntity->Defense -= floor(damage / ptrEntity->Defense);
 	}
 
 	if (ptrEntity->Defense < 0)
--- SP1Framework/Player.cpp
+++ SP1Framework/Player.cpp
@@ -20,8 +20,8 @@ void Player::Attack(Entity* ptrEntity, Console& g_Console) {
 		ptrEntity->HP -= damage;
 
 	if (ptrEntity->Defense != 0) {
-		ptrEntity->HP -= damage * 0.5;
-		ptrEntity->Defense -= damage * 0.5;
+		ptrEntity->HP -= floor(damage / ptrEntity->Defense);
+		ptrEntity->Defense -= floor(damage / ptrEntity->Defense);
 	}
 
 	if (ptrEntity->Defense < 0)
