7460cc72cabb79ea5aee8a59223edd691ef38f50
Fou <therealjo3d3su@gmail.com>
2020-08-29 16:18:16 +0800

    :ablobspin:

--- SP1Framework/renderBattle.cpp
+++ SP1Framework/renderBattle.cpp
@@ -4,7 +4,7 @@ void renderBattle(double& dt, Console& g_Console, Player& plr, Enemy& enemy, int
     renderCharacterSprite(g_Console, 5, 15, plr, dt);
     renderCharacterDamage(g_Console, plr, enemy.Damage);
     if ((enemy.enemy_no == 3) && (level_here == 12))
-        renderblob(g_Console, 82, 7, false);
+        renderblob(g_Console, 82, 7, true);
     else
         renderEnemySprite(g_Console, 113, 8, enemy, level_here);
     renderEnemyDamage(g_Console, enemy, plr);
